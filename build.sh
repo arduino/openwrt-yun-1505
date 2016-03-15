d0f0cbba85cd49bc7e6dd1b3d22cf3268e58cf4a
Martino Facchin <m.facchin@arduino.cc>
2016-03-15 17:47:56 +0100

    version 1.6.1, automatic job detection

--- build.sh
+++ build.sh
@@ -20,25 +20,20 @@ while getopts 'acshj:' flag; do
                 c)
 			export CLEAN=1
                         ;;
-		j)
-			shift
-                        if test $# -gt 0; then
-                                export JOBS=$1
-			fi
-			shift
-			;;
 		*)	
 			export COMPILEALL=0
 			export CLEAN=0
-			export JOBS=1
+			export JOBS=`ls -d /sys/devices/system/cpu/cpu[[:digit:]]* | wc -w `
 			;;
         esac
 done
 
+echo $CLEAN
+echo $JOBS $EXTRAFLAGS
+exit 1
+
 if [ x$CLEAN == x1 ]; then
 make clean
-fi
-
 ./scripts/feeds uninstall -a
 ./scripts/feeds update -a
 ./scripts/feeds install -a -d m
@@ -49,6 +44,7 @@ rm -rf ./package/feeds/packages/rng-tools
 rm -rf ./package/feeds/packages/avrdude
 ln -s ../../../feeds/arduino/rng-tools ./package/feeds/arduino/rng-tools
 ln -s ../../../feeds/arduino/avrdude ./package/feeds/arduino/avrdude
+fi
 
 if [ x$COMPILEALL == x1 ]; then
 cp config.full .config
@@ -58,5 +54,5 @@ fi
 
 make oldconfig
 
-make -j$JOBS IGNORE_ERRORS=m $EXTRAFLAGS
+make IGNORE_ERRORS=m $EXTRAFLAGS -j$JOBS
 
